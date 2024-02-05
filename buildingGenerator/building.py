49e52e19631be0dfa89625376d333a5d9034b335
tyul0529 <tyul0529@naver.com>
2024-02-05 22:43:00 +0900

    giving more details on villa

--- buildingGenerator/building.py
+++ buildingGenerator/building.py
@@ -30,7 +30,9 @@ class building:
         return canvas
 
 class villa(building):
-    roof_angle = 0.2
+    roof_angle = 0.17
+    width = 9
+    floor_heigth = 12
     def __init__(self, build_point, pitch, yaw, width_num, depth_num, floor_num, scale=1):
         super().__init__(build_point, pitch, yaw, width_num, depth_num, floor_num, scale)
 
@@ -39,16 +41,16 @@ class villa(building):
 
     def drawRoof(self, canvas):
         lines = []
-        top_point = self.getHeightPoint(self.build_point, self.scale*10*self.floor_num)
-        top_point = self.getRoofPoint(top_point, self.scale*8*self.depth_num/cos(self.roof_angle))
+        top_point = self.getHeightPoint(self.build_point, self.scale*self.floor_heigth*self.floor_num)
+        top_point = self.getRoofPoint(top_point, self.scale*self.width*self.depth_num/cos(self.roof_angle))
 
-        down_point = self.getRoofPoint(top_point, -self.scale*(8*self.depth_num/cos(self.roof_angle)+4))
+        down_point = self.getRoofPoint(top_point, -self.scale*(self.width*self.depth_num/cos(self.roof_angle)+5))
         mask_path = [top_point,
                     down_point,
-                    self.getWidthPoint(down_point, self.scale*12*self.width_num),
-                    self.getWidthPoint(top_point, self.scale*12*self.width_num)]
+                    self.getWidthPoint(down_point, self.scale*self.width*self.width_num),
+                    self.getWidthPoint(top_point, self.scale*self.width*self.width_num)]
         lines.append([top_point, down_point])
-        for i in range(12*self.width_num):
+        for i in range(self.width*self.width_num):
             top_point = self.getWidthPoint(top_point, self.scale)
             down_point = self.getWidthPoint(down_point, self.scale)
             lines.append([top_point, down_point])
@@ -56,24 +58,42 @@ class villa(building):
         canvas.registerMask(mask_path)
 
         return canvas
+    
+    def drawWindow(self, canvas, point):
+        window_heigth = 8
+        window_width = 4
+        lines = []
+        point1 = self.getHeightPoint(point, self.scale*window_heigth)
+        point2 = self.getWidthPoint(point, self.scale*window_width)
+        point3 = self.getWidthPoint(point1, self.scale*window_width)
+        lines.append([point, point1])
+        lines.append([point2, point3])
+        lines.append([point1, point3])
+        canvas.registerLineSegs(lines)
+        return canvas
 
+    def drawWindows(self, canvas, build_point):
+        for index in range(self.width_num):
+            canvas = self.drawWindow(canvas, self.getWidthPoint(build_point, self.scale*self.width*index+2))
 
+        return canvas
 
     def draw(self, canvas):
         canvas = self.drawRoof(canvas)
 
         lines = []
         point1 = self.build_point
-        lines.append([point1, self.getHeightPoint(point1, self.scale*10*self.floor_num)])
-        point1 = self.getWidthPoint(self.build_point, self.scale*12*self.width_num)
+        lines.append([point1, self.getHeightPoint(point1, self.scale*self.floor_heigth*self.floor_num)])
+        point1 = self.getWidthPoint(self.build_point, self.scale*self.width*self.width_num)
+        lines.append([point1, self.getHeightPoint(point1, self.scale*self.floor_heigth*self.floor_num)])
+        point1 = self.getDepthPoint(self.build_point, self.scale*self.width*self.depth_num)
+        lines.append([point1, self.getHeightPoint(point1, self.scale*(self.floor_heigth*self.floor_num+tan(self.roof_angle)*self.width*self.depth_num))])
         lines.append([self.build_point, point1])
-        lines.append([point1, self.getHeightPoint(point1, self.scale*10*self.floor_num)])
-        point1 = self.getDepthPoint(self.build_point, self.scale*8*self.depth_num)
-        lines.append([point1, self.getHeightPoint(point1, self.scale*(10*self.floor_num+tan(self.roof_angle)*8*self.depth_num))])
-        lines.append([self.build_point, point1])
 
-
-
+        for floor in range(self.floor_num):
+            point1 = self.getHeightPoint(self.build_point, self.scale*self.floor_heigth*floor)
+            lines.append([point1, self.getWidthPoint(point1, self.scale*self.width*self.width_num)])
+            canvas = self.drawWindows(canvas, point1)
 
         canvas.registerLineSegs(lines)
         return canvas
